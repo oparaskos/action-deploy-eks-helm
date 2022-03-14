ef3e5167cb5fa9600eac5f12322ac22ec1b71209
Oliver Paraskos <oliver@tryflux.com>
2022-03-14 11:29:55 +0000

    again

--- deploy.sh
+++ deploy.sh
@@ -29,10 +29,11 @@ if [ -n "${HELM_PLUGINS}" ]; then
     done
 fi
 
-helm repo update
-
 # Checking to see if a repo URL is in the path, if so add it or update.
 if [ -n "${HELM_REPOSITORY}" ]; then
+    helm repo add "${HELM_REPOSITORY}"
+    helm repo update
+
     HELM_CHART_NAME=${DEPLOY_CHART_PATH%/*}
     CHART_REPO_EXISTS=$(helm repo list | grep ^${HELM_CHART_NAME})
 
